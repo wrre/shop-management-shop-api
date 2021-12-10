5662b752a2d82ee58a1ef90c8f0fb7b577d2c170
Peggy Liu <fnoihtlanl@gmail.com>
2021-12-10 17:29:45 +0800

    add health api

---
+++ src/index.js
@@ -0,0 +1,18 @@
+// eslint-disable-next-line import/newline-after-import
+import './set-env';
+
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+app.use((req, res, next) => {
+  console.log(req.method, req.originalUrl, req.body);
+  next();
+});
+
+app.get('/health', (req, res) => {
+  res.send('ok');
+});
+
+app.listen(process.env.SERVICE_API_PORT);
---
+++ src/set-env.js
@@ -0,0 +1,3 @@
+import * as dotenv from 'dotenv-safe';
+
+dotenv.config();
