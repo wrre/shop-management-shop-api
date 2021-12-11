9f266bfad93b735f3bfb4df2a00a410e391faf2d
Peggy Liu <fnoihtlanl@gmail.com>
2021-12-11 22:01:07 +0800

    add cors and 404

--- src/index.js
+++ src/index.js
@@ -3,25 +3,31 @@ import './set-env';
 import './models';
 
 import express from 'express';
-import fs from "fs";
+import fs from 'fs';
 import jwt from 'express-jwt';
-import {shopRouter} from "../routers";
+import cors from 'cors';
+import { shopRouter } from '../routers';
 
-const {JWT_PUBLIC_KEY_PATH, SERVICE_API_PORT} = process.env
+const { JWT_PUBLIC_KEY_PATH, SERVICE_API_PORT } = process.env;
 
 const jwtPublicKey = fs.readFileSync(JWT_PUBLIC_KEY_PATH);
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use((req, res, next) => {
   console.log(req.method, req.originalUrl, req.body);
   next();
 });
-app.use(jwt({ secret: jwtPublicKey, algorithms: ['RS256']}).unless({path: ['/health']}));
-app.use(function (err, req, res, next) {
+app.use(
+  jwt({ secret: jwtPublicKey, algorithms: ['RS256'] }).unless({
+    path: ['/health'],
+  }),
+);
+app.use((err, req, res, next) => {
   if (err.name === 'UnauthorizedError') {
-    res.sendStatus(403)
+    res.sendStatus(403);
   }
 });
 
@@ -30,5 +36,9 @@ app.get('/health', (req, res) => {
 });
 
 app.use('/shops', shopRouter);
+
+app.use((req, res, next) => {
+  res.sendStatus(404);
+});
 
 app.listen(SERVICE_API_PORT);
