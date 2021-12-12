792b178220d15061cc1162823c0b94b5fc29ab6e
Peggy Liu <fnoihtlanl@gmail.com>
2021-12-12 23:37:06 +0800

    setup https

--- src/index.js
+++ src/index.js
@@ -6,9 +6,16 @@ import express from 'express';
 import fs from 'fs';
 import jwt from 'express-jwt';
 import cors from 'cors';
+import https from 'https';
 import { shopRouter } from '../routers';
 
-const { JWT_PUBLIC_KEY_PATH, SERVICE_API_PORT } = process.env;
+const {
+  NODE_ENV,
+  JWT_PUBLIC_KEY_PATH,
+  SERVICE_API_PORT,
+  SSL_PRIVATE_KEY_PATH,
+  SSL_CERTIFICATE_PATH,
+} = process.env;
 
 const jwtPublicKey = fs.readFileSync(JWT_PUBLIC_KEY_PATH);
 
@@ -41,4 +48,14 @@ app.use((req, res, next) => {
   res.sendStatus(404);
 });
 
-app.listen(SERVICE_API_PORT);
+if (NODE_ENV === 'local') {
+  app.listen(SERVICE_API_PORT);
+} else {
+  const privateKey = fs.readFileSync(SSL_PRIVATE_KEY_PATH);
+  const certificate = fs.readFileSync(SSL_CERTIFICATE_PATH);
+
+  const credentials = { key: privateKey, cert: certificate };
+
+  const httpsServer = https.createServer(credentials, app);
+  httpsServer.listen(SERVICE_API_PORT);
+}
